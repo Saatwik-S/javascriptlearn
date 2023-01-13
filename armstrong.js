e02b2470aea1a416b83511037e850ff262fd036d
Saatwik-S <saatwik_s@mckinsey.com>
2023-01-13 15:53:36 +0530

    opt: Modified functions

--- armstrong.js
+++ armstrong.js
@@ -1,22 +1,20 @@
 const num = 9474;
 let ans = 0;
 
-function isArmstrong(num) {
+const isArmstrong = (num) => {
     const len = num.toString().length;
     num.toString().split('').forEach(e => {
-        const converted = parseInt(e);
-        let tempAns = Math.pow(converted, len);
-        ans += tempAns;
+        ans += Math.pow(parseInt(e), len);
     });
     return ans === num;
 }
 
 console.log(isArmstrong(num) ? 'Armstrong' : 'Not Armstrong')
 
-const isPrime  = (num) => {
-    if(num < 2) return true;
-    for(let i = 2; i <= parseInt(Math.sqrt(num)) ;i++){
-        if(num % i == 0) {
+const isPrime = (num) => {
+    if (num < 2) return true;
+    for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+        if (num % i == 0) {
             return false;
         }
     }
